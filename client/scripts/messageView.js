2652b045a801286c756ee3e9d44910155c7e5fd1
Tyna Nguyen <ngtyna@gmail.com>
2020-10-16 19:38:23 -0500

    Refactored the render template function to render information in proper format

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -1,12 +1,31 @@
 var MessageView = {
 
   render: _.template(`
-      <!--
       <div class="chat">
-        <div class="username"></div>
-        <div></div>
-      </div>
-      -->
+        <div class="username"> <%=username %> </div>
+        <div class="text"> <%=text %> </div>
+        <div class="roomname"><%= roomname %></div>
+        </div>
     `)
-
 };
+
+/*
+From underscorejs:
+  var compiled = _.template("hello: <%= name %>");
+  compiled({name: 'moe'});
+  => "hello: moe"
+
+  => let message = {
+    username:
+    text:
+    roomname:
+  }
+
+From gLearn:
+- message objects sent to the parse server should be in this format:
+  var message = {
+    username: 'shawndrost',
+    text: 'trololo',
+    roomname: '4chan'
+  };
+*/
