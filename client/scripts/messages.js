7f1c738406e74fdff3c4b0a6d14db9d1dd8cdf0b
Tyna Nguyen <ngtyna@gmail.com>
2020-10-16 19:34:30 -0500

    Add a data property to Messages to contain all data

--- client/scripts/messages.js
+++ client/scripts/messages.js
@@ -1,4 +1,13 @@
 var Messages = {
 
+  _data: []
 
 };
+
+/*
+
+- create a data property set to an array (to contain each message?)
+- define how you want to store your messages (data: [])
+- define methods which allow you to retrieve from, add to and interact w messages
+
+*/
