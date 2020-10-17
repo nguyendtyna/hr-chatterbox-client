9d15b7d73550b8085badb6ff593e9470d3c5c89d
Tyna Nguyen <ngtyna@gmail.com>
2020-10-16 19:52:24 -0500

    Understand the function of GET and POST requests

--- client/scripts/parse.js
+++ client/scripts/parse.js
@@ -3,8 +3,8 @@ var Parse = {
   server: `http://parse.${window.CAMPUS}.hackreactor.com/chatterbox/classes/messages`,
 
   create: function (message, successCB, errorCB = null) {
+    // Create and store the message to the server
     $.ajax({
-      // This is the url you should use to communicate with the parse API server.
       url: Parse.server,
       type: 'POST',
       data: JSON.stringify(message),
@@ -13,13 +13,13 @@ var Parse = {
         console.log('chatterbox: Message sent');
       },
       error: function (data) {
-        // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
         console.error('chatterbox: Failed to send message', data);
       }
     });
   },
 
   readAll: function (successCB, errorCB = null) {
+    // Fetch all messages from the server
     $.ajax({
       url: Parse.server,
       type: 'GET',
