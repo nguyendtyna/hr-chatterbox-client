00a995bff87525d2eac368c660d0234862987411
Tyna Nguyen <ngtyna@gmail.com>
2020-10-17 12:21:14 -0500

    Implement an event handler to send a message upon clicking submit

--- client/scripts/formView.js
+++ client/scripts/formView.js
@@ -9,8 +9,22 @@ var FormView = {
   handleSubmit: function (event) {
     // Stop the browser from submitting the form
     event.preventDefault();
-
     console.log('click!');
+
+    // create a message template
+    let message = {
+      username: App.username,
+      text: $('#message').val(),
+      roomname: '#BestRoomEver'
+    }
+
+    // create message to be stored to the server
+    Parse.create(message);
+
+    // console.log(`This is the message: ${message}`);
+    // console.log('message.val:', $('#message').val(message));
+    // <input type="text" name="message" id="message"/>
+    // <input type="submit" name="submit" class="submit"/>
   },
 
   setStatus: function (active) {
